22209430108593cc6469fb5be7462c6d87c217d1
ap-bruh-2005 <94681692+ap-bruh-2005@users.noreply.github.com>
2022-01-22 14:46:04 +0530

    Update actions.py

--- chatbot/actions/actions/actions.py
+++ chatbot/actions/actions/actions.py
@@ -1,8 +1,6 @@
 # This files contains your custom actions which can be used to run
 # custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+
 
 from typing import Any, Text, Dict, List
 #
